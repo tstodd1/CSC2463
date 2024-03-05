5a3bb47cb4dabfebfbc4939e23ea5ef7f4d67d44
tstodd1 <129894495+tstodd1@users.noreply.github.com>
2024-03-04 22:59:28 -0600

    death

--- Bugs/sketch.js
+++ Bugs/sketch.js
@@ -1,6 +1,7 @@
 let bgImage;
 let bugImages = [];
-let imageURL = "https://tstodd1.github.io/CSC2463/Assets/"
+let bugSprites;
+let imageURL = "https://tstodd1.github.io/CSC2463/Assets/";
 let testBug, walk, death;
 
 function preload() {
@@ -8,24 +9,35 @@ function preload() {
   //  BugMove2 = loadImage("Assets/BUG-2.png.png")
   //  Bug_Squish = loadImage("Assets/BUG-3.png.png")
   bgImage = loadImage(imageURL + "floor.jpg");
+  bugSprite = loadImage(imageURL + "bugSheet.png")
   
     for (let i = 0; i < 3; i++) {
       //bugImages[i] = loadImage("Assets/bugMove.png" + "Assets/bugMove2.png" + "Assets/bugDeath.png");
       //bugImages[i] = loadImage("https://tstodd1.github.io/CSC2463/Assets/bugMove.png");
-      bugImages[i] = loadImage(imageURL + "bugSprite_" + (i+1) + ".png");
+      //bugImages[i] = loadImage(imageURL + "bugSprite_" + (i+1) + ".png");
     }
   }
 
 function setup() {
   createCanvas(800, 800);
   
-  testBug = createSprite(400, 400, 320, 320);
-  walk = testBug.addAnimation("walk", bugImages[0], bugImages[1], bugImages[0],);
-  walk.frameDelay = 8;
+  //testBug = createSprite(400, 400, 320, 320);
+  //walk = testBug.addAnimation("walk", bugImages[0], bugImages[1], bugImages[0],);
+  //walk.frameDelay = 8;
+  testBug = new Sprite();
+  testBug.img = (imageURL + "bugSprite_1.png")
+  testBug.addAni('walk', imageURL + "bugSprite_1.png", 2);
+  testBug.addAni('death', imageURL + "bugSprite_3.png", 1);
+  //testBug = new SpriteAnimation(imageURL + "bugSprite_1.png");
+  //walk = loadAni(imageURL + "bugSprite_1.png", 2);
+  //walk.frameDelay = 8;
+
+  //walk.onMousePressed = function() {
+  //  walk = loadAni(imageURL + "bugSprite_3", 1);
 
 }
 
 function draw() {
+  clear();
   background(bgImage);
-  drawSprite(testBug);
 }
