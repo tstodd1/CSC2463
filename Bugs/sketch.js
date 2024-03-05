6727119660c8f4d35293977e66068e58ab9c7c1a
tstodd1 <129894495+tstodd1@users.noreply.github.com>
2024-03-04 21:47:48 -0600

    big bugs

--- Bugs/sketch.js
+++ Bugs/sketch.js
@@ -1,25 +1,30 @@
+let bgImage;
 let bugImages = [];
-let testBug, walking, squish;
+let imageURL = "https://tstodd1.github.io/CSC2463/Assets/"
+let testBug;
 
 function preload() {
-// bugMove = loadImage("Assets/BUG-1.png.png")
-//  BugMove2 = loadImage("Assets/BUG-2.png.png")
-//  Bug_Squish = loadImage("Assets/BUG-3.png.png")
-
-  for (let i = 0; i < 3; i++) {
-    bugImages[i] = loadImage("Assets/bugMove.png" + "Assets/bugMove2.png" + "Assets/bugDeath.png");
+  // bugMove = loadImage("Assets/BUG-1.png.png")
+  //  BugMove2 = loadImage("Assets/BUG-2.png.png")
+  //  Bug_Squish = loadImage("Assets/BUG-3.png.png")
+  bgImage = loadImage(imageURL + "floor.jpg");
+  
+    for (let i = 0; i < 3; i++) {
+      //bugImages[i] = loadImage("Assets/bugMove.png" + "Assets/bugMove2.png" + "Assets/bugDeath.png");
+      //bugImages[i] = loadImage("https://tstodd1.github.io/CSC2463/Assets/bugMove.png");
+      bugImages[i] = loadImage(imageURL + "bugSprite_" + (i+1) + ".png");
+    }
   }
-}
 
 function setup() {
   createCanvas(800, 800);
 
-  testBug = createSprite(width / 2, height / 2, 32, 32);
-  testBug.addAnimation("walking", bugImages[0], bugImages[1], bugImages[0], bugImages[2]);
+  testBug = createSprite(width / 200, height / 200, 32, 32);
+  testBug.addAnimation("walk", bugImages[0], bugImages[1], bugImages[0],);
 
 }
 
 function draw() {
-  background(220);
+  background(bgImage);
   drawSprites();
 }
--- sketch.js
+++ sketch.js
@@ -12,13 +12,16 @@ function preload() {
     for (let i = 0; i < 3; i++) {
       //bugImages[i] = loadImage("Assets/bugMove.png" + "Assets/bugMove2.png" + "Assets/bugDeath.png");
       //bugImages[i] = loadImage("https://tstodd1.github.io/CSC2463/Assets/bugMove.png");
-      bugImages[i] = loadImage(imageURL + "bugSprite_" + (i + 1) + ".png");
+      bugImages[i] = loadImage(imageURL + "bugSprite_" + (i+1) + ".png");
 
     }
   }
 
 function setup() {
   createCanvas(800, 800);
+
+  testBug = createSprite(width / 2, height / 2, 32, 32);
+  testBug.addAnimation("walking", bugImages[0], bugImages[1], bugImages[2])  
 
 }
 
